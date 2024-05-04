303c6b292d24bdf4cfb020d1149e23c43b0a931d
shreya-kothari8 <kothari.shreya810@gmail.com>
2024-05-04 21:18:44 +0530

    string with positive number and , as delimiter

--- assessment.js
+++ assessment.js
@@ -1,1 +1,12 @@
-//Logic building Code
+function add(data)
+{
+    // Sata is the input given.
+    // Split based on delimeter "," which will result in a array of string element.
+    // map(Number) will convert string element to Number.
+    // reduce method helps to reduce on the basis of given operations.
+
+    return data.split(",").map(Number).reduce((a,b)=> {return a+b})
+}
+
+module.exports=add;
+
--- assessment.test.js
+++ assessment.test.js
@@ -1,1 +1,14 @@
-//Test Cases
+const add = require('./assessment');
+
+
+test("add 1 to equal 1", () => {
+  expect(add("1")).toBe(1);
+});
+
+test("add 0 to equal 0", () => {
+  expect(add("0")).toBe(0);
+});
+
+test("add 1+2+3+4 to equal 10", () => {
+  expect(add("1,2,3,4")).toBe(10);
+});
