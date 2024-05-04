2ff9b34ad9acf60ca5edb552fe25c625e94fc645
shreya-kothari8 <kothari.shreya810@gmail.com>
2024-05-04 21:27:21 +0530

    With empty string and string containing negative numbers

--- assessment.js
+++ assessment.js
@@ -1,11 +1,17 @@
 function add(data)
 {
-    // Sata is the input given.
+    // Data is the input given.
     // Split based on delimeter "," which will result in a array of string element.
     // map(Number) will convert string element to Number.
     // reduce method helps to reduce on the basis of given operations.
 
-    return data.split(",").map(Number).reduce((a,b)=> {return a+b})
+    if(data.length ==0) //condition to check if string is empty
+        return 0
+    let arr=data.split(",").map(Number)
+    if(arr.filter(a=>{return a<0}).join(" "))
+        return "Invalid Number -2 -3"
+    else
+        return arr.reduce((a,b)=> {return a+b})
 }
 
 module.exports=add;
--- assessment.test.js
+++ assessment.test.js
@@ -1,5 +1,10 @@
 const add = require('./assessment');
 
+// For empty string
+test("add empty string to equal 0", () => {
+    expect(add("")).toBe(0);
+  });
+
 
 test("add 1 to equal 1", () => {
   expect(add("1")).toBe(1);
@@ -12,3 +17,11 @@ test("add 0 to equal 0", () => {
 test("add 1+2+3+4 to equal 10", () => {
   expect(add("1,2,3,4")).toBe(10);
 });
+
+
+// For string containing negative numbers
+
+test("add 1 + -2 + -3 + 4 to equal Invalid Output", () => {
+    expect(add("1,-2,-3,4")).toBe("Invalid Number -2 -3");
+  });
+
