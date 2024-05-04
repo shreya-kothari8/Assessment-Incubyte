88ef1632ec89960e4af2298c49b1286d5d769bc0
shreya-kothari8 <kothari.shreya810@gmail.com>
2024-05-04 22:30:19 +0530

    With generic Test Cases

--- assessment.js
+++ assessment.js
@@ -1,18 +1,38 @@
-function add(data)
-{
-    // Data is the input given.
-    // Split based on delimeter "," which will result in a array of string element.
-    // map(Number) will convert string element to Number.
-    // reduce method helps to reduce on the basis of given operations.
+function add(data) {
+  // // Data is the input given.
+  // // Split based on delimeter "," which will result in a array of string element.
+  // // map(Number) will convert string element to Number.
+  // // reduce method helps to reduce on the basis of given operations.
 
-    if(data.length ==0) //condition to check if string is empty
-        return 0
-    let arr=data.split(",").map(Number)
-    if(arr.filter(a=>{return a<0}).join(" "))
-        return "Invalid Number -2 -3"
+  // if(data.length ==0) //condition to check if string is empty
+  //     return 0
+  // let arr=data.split(",").map(Number)
+  // if(arr.filter(a=>{return a<0}).join(" "))
+  //     return "Invalid Number -2 -3"
+  // else
+  //     return arr.reduce((a,b)=> {return a+b})
+
+  if (data.length == 0) return 0;
+
+  if (Number.isInteger(Number(data[data.length - 1]))) {
+    if (data.endsWith("\n")) {
+      return "Invalid Input";
+    }
+    let arr = data.match(/-?\d+/g).map(Number);
+    let negNumArr = arr
+      .filter((a) => {
+        return a < 0;
+      })
+      .join(" ");
+
+    if (negNumArr) return `Negative number not allowed ${negNumArr}`;
     else
-        return arr.reduce((a,b)=> {return a+b})
+      return arr.reduce((a, b) => {
+        return a + b;
+      });
+  } else {
+    return "Invalid Input";
+  }
 }
 
-module.exports=add;
-
+module.exports = add;
--- assessment.test.js
+++ assessment.test.js
@@ -1,27 +1,50 @@
-const add = require('./assessment');
+const add = require("./assessment");
 
-// For empty string
-test("add empty string to equal 0", () => {
-    expect(add("")).toBe(0);
-  });
+// // For empty string
+// test("add empty string to equal 0", () => {
+//     expect(add("")).toBe(0);
+//   });
 
+// test("add 1 to equal 1", () => {
+//   expect(add("1")).toBe(1);
+// });
 
-test("add 1 to equal 1", () => {
-  expect(add("1")).toBe(1);
+// test("add 0 to equal 0", () => {
+//   expect(add("0")).toBe(0);
+// });
+
+// test("add 1+2+3+4 to equal 10", () => {
+//   expect(add("1,2,3,4")).toBe(10);
+// });
+
+// // For string containing negative numbers
+
+// test("add 1 + -2 + -3 + 4 to equal Invalid Output", () => {
+//     expect(add("1,-2,-3,4")).toBe("Invalid Number -2 -3");
+//   });
+
+const items = [
+  { input: "", output: 0 },
+  { input: "0", output: 0 },
+  { input: "2", output: 2 },
+  { input: "-1", output: "Negative number not allowed -1" },
+  { input: "5,7", output: 12 },
+  { input: "-4,-3", output: "Negative number not allowed -4 -3" },
+  { input: "1,2,3,4,3,14,15", output: 42 },
+  {input:"kjdhskjfhjkf",output:"Invalid Input"},
+  {input:"1\n2,3",output:6},
+  {input:"1,\n",output:"Invalid Input"},
+  {input:"//",output:"Invalid Input"},
+  {input:"//;\n1;2",output:3},
+  { input: "1;2,3&4,3,14,15", output: 42 },
+   
+
+];
+
+describe("Test Cases", () => {
+  for (let i = 0; i < items.length; i++) {
+    it(`should  add ${items[i].input} to ${items[i].output}`, () => {
+      expect(add(items[i].input)).toBe(items[i].output);
+    });
+  }
 });
-
-test("add 0 to equal 0", () => {
-  expect(add("0")).toBe(0);
-});
-
-test("add 1+2+3+4 to equal 10", () => {
-  expect(add("1,2,3,4")).toBe(10);
-});
-
-
-// For string containing negative numbers
-
-test("add 1 + -2 + -3 + 4 to equal Invalid Output", () => {
-    expect(add("1,-2,-3,4")).toBe("Invalid Number -2 -3");
-  });
-
